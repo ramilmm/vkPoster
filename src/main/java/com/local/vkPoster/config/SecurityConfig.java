da8bde74b4e92a63121b74d2b89fe13fe22ae0f7
ramilmm <ramilmmk@gmail.com>
2019-03-10 01:13:00 +0300

    delete web config

--- src/main/java/com/local/vkPoster/config/SecurityConfig.java
+++ src/main/java/com/local/vkPoster/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SpringSecConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
